09020d691bf972d9acaca7c175a0da838c420e01
yoojin <yoojin9005@gmail.com>
2022-06-29 22:34:50 -0400

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.new_task_description.value);
+    form.reset();
+    })
+  })
+
+function handleToDo(todo) {
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+
+    btn.addEventListener('click', function handleDelete(e) {
+      e.target.parentNode.remove()
+    })
+    btn.textContent = 'x'
+    p.textContent = `${todo} `;
+
+    p.appendChild(btn);
+    document.querySelector('#tasks').appendChild(p)
+}
+
+
+
+  
